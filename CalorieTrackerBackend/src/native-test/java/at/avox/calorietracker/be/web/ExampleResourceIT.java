9213d9113ec285f8ae3d7b00071af620f6c7faaf
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-01-11 19:37:05 +0100

    fix: Removed unnecessary tests

--- CalorieTrackerBackend/src/native-test/java/at/avox/calorietracker/be/web/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package at.avox.calorietracker.be.web;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
